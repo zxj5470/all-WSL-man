0d3a6e423dca0cd87fdb4133f347ff3a5289a2ac
zxj5470 <zxj5470@foxmail.com>
2019-03-17 01:39:30 +0800

    add catalog

---
+++ trans.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+from string import Template
+if __name__ == "__main__":
+    f = open("catalog.txt",newline = "\n")
+    md = open("catalog.md","w",newline = "\n")
+    for line in f:
+        line=line[:-1]
+        current = f'[{line}](files/{line})\n'
+        md.write(current)
+    md.close()
+    f.close()
+    pass
